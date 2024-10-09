dc1b928551291f9c702d62e85fb5a5624cab460e
Yoon <49135657+Yoon6@users.noreply.github.com>
2024-10-09 23:25:41 +0900

    Title: 코드트리 빵, Time: 426ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 241009/코드트리 빵/codetree-mon-bread.py
+++ 241009/코드트리 빵/codetree-mon-bread.py
@@ -88,6 +88,7 @@ def move_to_store(player):
     player.x, player.y = x, y
     if x == tx and y == ty:
         player.state = State.FINISHED
+        maps[x][y] = -1
 
 
     """
@@ -151,6 +152,7 @@ def init(player):
 
     player.x, player.y = mx, my
     player.state = State.PLAYING
+    maps[mx][my] = -1
 
 
 def move(time):
@@ -161,15 +163,8 @@ def move(time):
         if players[i].state == State.PLAYING:
             move_to_store(players[i])
 
-    for i in range(m):
-        tx, ty = players[i].get_target_pos()
-        if players[i].x == tx and players[i].y == ty:
-            maps[tx][ty] = -1
-
     if time <= m:
         init(players[time-1])
-        x, y = players[time-1].x, players[time-1].y
-        maps[x][y] = -1
 
 
 def main():
